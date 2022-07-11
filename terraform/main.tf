b9cf3e9ba5cf1c65111084adef6ad9499b13d8e9
Rony Blum <blumron@amazon.com>
2022-07-11 18:56:25 +0300

    Second account provisioning

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,12 +11,40 @@ module "sandbox" {
   }
 
   account_tags = {
-    "Learn Tutorial" = "AFT"
+    "Stage" = "Sandbox"
   }
 
   change_management_parameters = {
-    change_requested_by = "HashiCorp Learn"
-    change_reason       = "Learn AWS Control Tower Account Factory for Terraform"
+    change_requested_by = "Rony"
+    change_reason       = "Provision Sandbox Account"
+  }
+
+  custom_fields = {
+    group = "non-prod"
+  }
+
+  account_customizations_name = "sandbox"
+}
+
+module "sandbox" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "blumron+sandbox_01@amazon.com"
+    AccountName               = "sandbox-01"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "blumron+sandbox_01@amazon.com"
+    SSOUserFirstName          = "Rony"
+    SSOUserLastName           = "Blum"
+  }
+
+  account_tags = {
+    "Stage" = "Sandbox"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Rony"
+    change_reason       = "Provision Sandbox Account"
   }
 
   custom_fields = {
