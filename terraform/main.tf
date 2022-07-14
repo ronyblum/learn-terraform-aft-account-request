669a628002bbe258732bdf99f546757bcc94bad0
Rony Blum <blumron@amazon.com>
2022-07-14 08:41:01 +0300

    Second Account Creation

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,34 +2,6 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "blumron+sandbox_aft@amazon.com"
-    AccountName               = "sandbox-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "blumron+sandbox_aft@amazon.com"
-    SSOUserFirstName          = "Sandbox"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Stage" = "Sandbox"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "Rony"
-    change_reason       = "Provision Sandbox Account"
-  }
-
-  custom_fields = {
-    group = "non-prod"
-  }
-
-  account_customizations_name = "sandbox"
-}
-
-module "sandbox" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
     AccountEmail              = "blumron+sandbox_01@amazon.com"
     AccountName               = "sandbox-01"
     ManagedOrganizationalUnit = "Sandbox"
